94d32d7271c076477a6a19e6be39eddbec28d4a7
Manuratnam <manuratnammj@gmail.com>
2026-04-07 05:46:14 +0530

    Create 16.num rev function.c

---
+++ 16.num rev function.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+// function to reverse number
+int reverse(int n)
+{
+    int rev = 0, rem;
+
+    while(n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    return rev;
+}
+
+int main()
+{
+    int n, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    result = reverse(n);
+
+    printf("Reversed number = %d", result);
+
+    return 0;
+}
